ef4c7a34a2c72c9a0c359a88708708c8fb5e18a9
Dankaroum <139598064+Dankaroum@users.noreply.github.com>
2023-10-18 02:19:23 +0200

    Add files via upload

--- 6-main.c
+++ 6-main.c
@@ -1,18 +1,15 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
+   * main - check the code for Holberton School students.
+    *
+     * Return: Always 0.
+      */
 int main(void)
 {
-	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
-	char *p;
+	    char *str;
 
-	p = cap_string(s);
-	printf("%s", p);
-	printf("%s", s);
-	return (0);
+	    str = "0123456789";
+	    puts2(str);	
+	    return (0);
 }
--- 7-main.c
+++ 7-main.c
@@ -1,18 +1,15 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
  * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
+    *
+     * Return: Always 0.
+      */
 int main(void)
 {
-	char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
-	char *p;
+	    char *str;
 
-	p = leet(s);
-	printf("%s", p);
-	printf("%s", s);
-	return (0);
+	        str = "0123456789";
+		    puts_half(str);
+		        return (0);
 }
